4e5430b5d62ac734e7dfade6b9466eb4aa7adae8
Mathieu <mrobert@w-inn.eu>
2017-06-21 17:58:39 +0200

    Updated review page with star review, added review link in single activity page

--- www/build/2.main.js
+++ www/build/2.main.js
@@ -1,14 +1,14 @@
 webpackJsonp([2],{
 
-/***/ 294:
+/***/ 296:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(6);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__splash__ = __webpack_require__(112);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "SplashModule", function() { return SplashModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__trip__ = __webpack_require__(113);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TripModule", function() { return TripModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -18,26 +18,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
-var SplashModule = (function () {
-    function SplashModule() {
+var TripModule = (function () {
+    function TripModule() {
     }
-    return SplashModule;
+    return TripModule;
 }());
-SplashModule = __decorate([
+TripModule = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__splash__["a" /* Splash */],
+            __WEBPACK_IMPORTED_MODULE_2__trip__["a" /* Trip */],
         ],
         imports: [
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__splash__["a" /* Splash */]),
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__trip__["a" /* Trip */]),
         ],
         exports: [
-            __WEBPACK_IMPORTED_MODULE_2__splash__["a" /* Splash */]
+            __WEBPACK_IMPORTED_MODULE_2__trip__["a" /* Trip */]
         ]
     })
-], SplashModule);
+], TripModule);
 
-//# sourceMappingURL=splash.module.js.map
+//# sourceMappingURL=trip.module.js.map
 
 /***/ })
 
